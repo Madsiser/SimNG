b5ee7c9d0e2d42eac941b525e6db23b18babc485
Krystian Madej <madsiserpl@interia.pl>
2024-12-19 15:08:02 +0100

    Updated Position class

--- src/main/java/simulation/engine/Position.java
+++ src/main/java/simulation/engine/Position.java
@@ -1,12 +1,20 @@
 package simulation.engine;
 
 public class Position {
-    private final double x;
-    private final double y;
+    private double x;
+    private double y;
 
     public Position(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
     }
 
     public double getX() {
