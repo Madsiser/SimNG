ea5e39129df63279a3ad7db4713edc756ba6b434
bilosek <kamil.biloss@gmail.com>
2025-01-27 12:24:33 +0100

    refactor: sim-force-type

--- src/main/java/pl/simNG/SimForceType.java
+++ src/main/java/pl/simNG/SimForceType.java
@@ -1,5 +1,10 @@
 package pl.simNG;
 
+/**
+ * Enum reprezentujący strony konfliktu w symulacji.
+ * BLUFORCE - reprezentuje siły przyjazne (Blue Force).
+ * REDFORCE - reprezentuje siły wroga (Red Force).
+ */
 public enum SimForceType {
     BLUFORCE,
     REDFORCE
