10c2b599bffc860055a1ae5e597626acdd03cb48
bilosek <kamil.biloss@gmail.com>
2025-01-27 12:22:42 +0100

    refactor: added documentation in command, command-type, sim-commander in package commands

--- src/main/java/pl/simNG/commands/CommandType.java
+++ src/main/java/pl/simNG/commands/CommandType.java
@@ -1,7 +1,11 @@
 package pl.simNG.commands;
 
+/** Enum reprezentujący typy rozkazów w symulacji. */
 public enum CommandType {
+    /** Rozkaz poruszania się. */
     MOVE,
+    /** Rozkaz ataku. */
     ATTACK,
+    /** Rozkaz obrony. */
     DEFEND
 }
--- src/main/java/pl/simNG/commands/SimCommander.java
+++ src/main/java/pl/simNG/commands/SimCommander.java
@@ -6,15 +6,30 @@ import pl.simNG.scheduler.SimExecutionScheduler;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Klasa zarządzająca rozkazami w symulacji.
+ * Odpowiada za dystrybucję rozkazów do jednostek (grup) oraz monitorowanie ich realizacji.
+ */
 public class SimCommander extends SimExecutionScheduler {
+    /** Kolejka rozkazów do wykonania. */
     protected List<Command> commandQueue = new ArrayList<>();
+    /** Lista jednostek (grup) zarządzanych przez SimCommandera. */
     protected List<SimGroup> groups = new ArrayList<>();
+    /** Bieżący rozkaz przypisany do grup jednostek. */
     public Command currentCommand = null;
 
+    /**
+     * Konstruktor klasy SimCommander.
+     * Dodaje zadanie głównej pętli kontrolnej odpowiedzialnej za zarządzanie rozkazami.
+     */
     SimCommander(){
         addTask(this::mainFx,1);
     }
 
+    /**
+     * Główna pętla kontrolna odpowiedzialna za zarządzanie rozkazami.
+     * Sprawdza, czy wszystkie jednostki wykonały bieżący rozkaz i przypisuje nowy rozkaz.
+     */
     private void mainFx(){
         if (allDone()){
             assignNewCommand();
@@ -25,14 +40,26 @@ public class SimCommander extends SimExecutionScheduler {
 
     }
 
+    /**
+     * Dodaje grupę jednostek do zarządzania przez SimCommandera.
+     * @param group grupa jednostek
+     */
     public void addGroups(SimGroup group) {
         groups.add(group);
     }
 
+    /**
+     * Zwraca bieżący rozkaz.
+     * @return aktualny rozkaz
+     */
     public Command getCurrentCommand() {
         return currentCommand;
     }
 
+    /**
+     * Przypisuje nowy rozkaz do kolejki i automatycznie ustawia go jako bieżący, jeśli brak aktywnego rozkazu.
+     * @param command nowy rozkaz
+     */
     public void assignCommand(Command command) {
         commandQueue.add(command);
         if (currentCommand == null) {
@@ -41,10 +68,18 @@ public class SimCommander extends SimExecutionScheduler {
         assignNewCommand();
     }
 
+    /**
+     * Dodaje rozkaz do kolejki.
+     * @param command nowy rozkaz
+     */
     public void addCommand(Command command){
         commandQueue.add(command);
     }
 
+    /**
+     * Sprawdza, czy wszystkie grupy jednostek zakończyły wykonywanie bieżącego rozkazu.
+     * @return true, jeśli wszystkie jednostki zakończyły rozkaz, false w przeciwnym razie
+     */
     private boolean allDone(){
         boolean allDone = true;
         for (SimGroup g : groups) {
@@ -56,6 +91,9 @@ public class SimCommander extends SimExecutionScheduler {
         return allDone;
     }
 
+    /**
+     * Przypisuje nowy rozkaz z kolejki jako bieżący i rozsyła go do wszystkich grup jednostek.
+     */
     private void assignNewCommand(){
         currentCommand = commandQueue.isEmpty() ? null : commandQueue.remove(0);
         for (SimGroup g : groups) {
